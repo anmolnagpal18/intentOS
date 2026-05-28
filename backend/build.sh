84cb3c26ab2d513bb547c90a354681de97726e24
anmolnagpal18 <anmoln507@gmail.com>
2026-05-28 21:21:58 +0530

    Automate superuser creation during Render blueprint build

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create a superuser on the free tier of Render
+python manage.py shell -c "from django.contrib.auth.models import User; import os; User.objects.filter(username=os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')).exists() or User.objects.create_superuser(os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin'), os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com'), os.getenv('DJANGO_SUPERUSER_PASSWORD', 'admin123'))"
+
